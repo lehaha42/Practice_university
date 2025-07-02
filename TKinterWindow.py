f3f6758dfea05a2abf788f5369ca6235228b0211
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2025-07-02 13:57:50 +1000

    screen clear button speed and length entry

--- TKinterWindow.py
+++ TKinterWindow.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from math import pi
 
 
 class TKinterWindow:
@@ -12,7 +13,9 @@ class TKinterWindow:
         button = tk.Button(self.root, text='поменять режим сетки', command=self.change_mode)
         button.grid(row=0, column=0)
         button = tk.Button(self.root, text='применить', command=self.update)
-        button.grid(row=4, column=0)
+        button.grid(row=6, column=0)
+        button = tk.Button(self.root, text='очистить', command=self.clear)
+        button.grid(row=6, column=1)
 
         self.label1 = tk.Label(self.root, text=f'текущий режим: {['декартова', 'полярная'][self.resource.mode]}')
         self.label1.grid(row=0, column=1)
@@ -23,11 +26,19 @@ class TKinterWindow:
         label.grid(row=2, column=0)
         label = tk.Label(self.root, text='b = ')
         label.grid(row=3, column=0)
+        label = tk.Label(self.root, text='speed = ')
+        label.grid(row=4, column=0)
+        label = tk.Label(self.root, text='length = ')
+        label.grid(row=5, column=0)
 
         self.entry1 = tk.Entry(self.root)
         self.entry1.grid(row=2, column=1)
         self.entry2 = tk.Entry(self.root)
         self.entry2.grid(row=3, column=1)
+        self.entry3 = tk.Entry(self.root)
+        self.entry3.grid(row=4, column=1)
+        self.entry4 = tk.Entry(self.root)
+        self.entry4.grid(row=5, column=1)
 
     def change_mode(self):
         self.resource.mode = 1 - self.resource.mode
@@ -36,11 +47,28 @@ class TKinterWindow:
     def update(self):
         try:
             a = float(self.entry1.get())
-            b = float(self.entry2.get())
             self.resource.a = a
+        except Exception:
+            pass
+        try:
+            b = float(self.entry2.get())
             self.resource.b = b
         except Exception:
             pass
+        try:
+            speed = float(self.entry3.get())
+            self.resource.speed = speed
+        except Exception:
+            pass
+        try:
+            length = float(self.entry4.get())
+            self.resource.length = length * pi
+        except Exception:
+            pass
+
+    def clear(self):
+        self.resource.a = 0
+        self.resource.b = 0
 
     def on_close(self):
         self.resource.terminate = True
