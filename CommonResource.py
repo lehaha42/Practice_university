1ab7a4642252a7934c5d594bbd6192c3cda34683
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2025-07-02 13:58:01 +1000

    extra vars

--- CommonResource.py
+++ CommonResource.py
@@ -5,5 +5,7 @@ class CommonResource:
     def __init__(self):
         self.a = 0
         self.b = 0
+        self.speed = 1
+        self.length = 10
         self.mode = 0
         self.terminate = False
